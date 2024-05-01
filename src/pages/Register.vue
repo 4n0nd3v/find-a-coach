e0e9950ec128c026d21f3ae8ff3627dad77c6010
Rafael Longo <rafaelongo45@gmail.com>
2024-05-01 00:57:42 -0300

    feat: includes validation for form to register coach

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,36 +1,46 @@
 <template>
   <BaseCard title="Register as a coach now!">
-    <form @submit.prevent="registerCoach()">
-      <label for="fistname">First name</label>
-      <input type="text" name="firstname" v-model="firstname">
+    <form @submit.prevent="registerCoach">
+      <section :class="{invalid: !firstname.isValid}">
+        <label for="fistname">First name</label>
+        <input type="text" name="firstname" v-model.trim="firstname.val" @blur="clearValidity('firstname')">
+      </section>
 
-      <label for="lastname">Last name</label>
-      <input type="text" name="lastname" v-model="lastname">
+      <section :class="{invalid: !lastname.isValid}">
+        <label for="lastname">Last name</label>
+        <input type="text" name="lastname" v-model.trim="lastname.val" @blur="clearValidity('lastname')">
+      </section>
 
-      <label for="description">Description</label>
-      <textarea class="description" type="text" name="description" v-model="description" />
+      <section :class="{invalid: !description.isValid}">
+        <label for="description">Description</label>
+        <textarea class="description" type="text" name="description" v-model.trim="description.val" @blur="clearValidity('description')"/>
+      </section>
 
-      <label for="price">Hourly Rate</label>
-      <input type="number" name="price" v-model="price">
+      <section :class="{invalid: !price.isValid}">
+        <label for="price">Hourly Rate</label>
+        <input type="number" name="price" v-model="price.val" @blur="clearValidity('price')">
+      </section>
 
       <p for="skills">Areas of Expertise</p>
 
-      <div class="skills">
+      <section :class="{invalid: !skills.isValid}">
+        <div class="skills">
         <div class="check">
           <label for="frontend">Frontend</label>
-          <input @click="selectSkill($event)" type="checkbox" name="frontend" value="frontend" v-model="skills"> 
+          <input @click="selectSkill($event)" type="checkbox" name="frontend" value="frontend" v-model="skills.val" @blur="clearValidity('skills')"> 
         </div>
 
         <div class="check">
           <label for="backend">Backend</label>
-          <input @click="selectSkill($event)" type="checkbox" name="backend" value="backend" v-model="skills"> 
+          <input @click="selectSkill($event)" type="checkbox" name="backend" value="backend" v-model="skills.val" @blur="clearValidity('skills')"> 
         </div>
 
         <div class="check">
           <label for="career">Career Development</label>
-          <input @click="selectSkill($event)" type="checkbox" name="carreer" value="career" v-model="skills">
+          <input @click="selectSkill($event)" type="checkbox" name="career" value="career" v-model="skills.val" @blur="clearValidity('skills')">
         </div> 
       </div>
+      </section>
 
       <button type="submit">Register</button>
     </form>
@@ -42,24 +52,76 @@ import BaseCard from '../components/ui/BaseCard';
  export default {
   data() {
     return {
-      firstname: null,
-      lastname: null,
-      description: null,
-      price: null,
-      skills: []
+      firstname: {
+        val: '',
+        isValid: true
+      },
+      lastname: {
+        val: '',
+        isValid: true
+      },
+      description: {
+        val: '',
+        isValid: true
+      },
+      price: {
+        val: null,
+        isValid: true
+      },
+      skills: {
+        val: [],
+        isValid: true
+      },
+      isFormValid: true
     }
   },
   components: {
     BaseCard
   },
   methods: {
+    clearValidity(input) {
+      this[input].isValid = true;
+    },
+    validateForm() {
+      if(this.firstname.val === '') {
+        this.firstname.isValid = false;
+        this.isFormValid = false
+      }
+
+      if(this.lastname.val === '') {
+        this.lastname.isValid = false;
+        this.isFormValid = false
+      }
+
+      if(this.description.val === '') {
+        this.description.isValid = false;
+        this.isFormValid = false
+      }
+
+      if(this.price.val === null || this.price.val < 0) {
+        this.price.isValid = false;
+        this.isFormValid = false
+      }
+
+      if(this.skills.val.length === 0) {
+        this.skills.isValid = false;
+        this.isFormValid = false
+      }
+    },
     async registerCoach() {
+      this.isFormValid = true;
+      this.validateForm();
+
+      if(!this.isFormValid) {
+        return;
+      }
+      console.log('aqq')
       const coachData = {
-        firstname: this.firstname,
-        lastname: this.lastname,
-        description: this.description,
-        price: this.price,
-        skills: this.skills
+        firstname: this.firstname.val,
+        lastname: this.lastname.val,
+        description: this.description.val,
+        price: this.price.val,
+        skills: this.skills.val
       }
       
       await fetch('https://find-a-coach-c82ef-default-rtdb.firebaseio.com/coaches.json', {
@@ -70,13 +132,13 @@ import BaseCard from '../components/ui/BaseCard';
     selectSkill(event) {
       const value = event.target.value;
       
-      const index = this.skills.indexOf(value); 
+      const index = this.skills.val.indexOf(value); 
 
       if (index === -1) {
-        return this.skills.push(value);
+        return this.skills.val.push(value);
       }
 
-      this.skills.splice(index, 1);
+      this.skills.val.splice(index, 1);
     }
   }
  }
@@ -91,10 +153,14 @@ import BaseCard from '../components/ui/BaseCard';
     margin-bottom: 20px;
     position: relative;
 
-    p {
+    p, label {
       margin-bottom: 5px;
       font-weight: 800;
       font-size: 16px;
+    }
+
+    label {
+      font-weight: 500;
     }
 
     input, textarea {
@@ -148,5 +214,20 @@ import BaseCard from '../components/ui/BaseCard';
     button:hover {
       cursor: pointer;
     }
+  }
+  
+  .invalid {
+    input, label {
+    color: red;
+  }
+
+  input {
+    border: 1px solid red;
+  }
+  }
+
+  section {
+    display: flex;
+    flex-direction: column;
   }
 </style>
