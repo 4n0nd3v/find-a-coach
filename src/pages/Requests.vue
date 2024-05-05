a8b00b0163509daddce96446a82c9ff5a9d7ab86
Rafael Longo <rafaelongo45@gmail.com>
2024-05-05 13:16:15 -0300

    alters style for requests page

--- src/pages/Requests.vue
+++ src/pages/Requests.vue
@@ -1,10 +1,14 @@
 <template>
-  <div v-if="requests.length !== 0">
-    <BaseCard v-for="request of requests" :key="request.id" class="request">
-      <h1>{{ request.userEmail }}</h1>
-      <p >{{ request.message }}</p>
+  <BaseCard class="request">
+    <h1>Requests Received</h1>
+    <section v-if="requests.length !== 0">
+      <div v-for="request of requests" :key="request.id">
+        <p class="mail">teste@.com</p>
+        <p> erquest reste</p>
+      </div>
+    </section>
+    <h2 v-else>No requests yet!</h2>
   </BaseCard>
-  </div>
 </template>
 
 <script>
@@ -25,14 +29,38 @@ export default {
 <style scoped>
   .request {
     h1{
-      margin: 15px 20px;
-      font-size: 16px;
+      text-align: center;
+      margin: 40px 0;
+      font-size: 24px;
       font-weight: 700;
     }
 
+    h2 {
+      margin: 30px;
+      text-align: center;
+      font-size: 16px;
+    }
+
+    div {
+      margin: 20px auto;
+      width: 80%;
+      border: 1px solid rgb(218, 218, 218);
+    }
+
+    div:last-child{
+      margin-bottom: 50px;
+    }
+
     p {
-      font-weight: 400;
-      margin: 20px;
+      margin: 15px;
+      margin-bottom: 20px;
+      font-weight: 500;
+    }
+
+    .mail {
+      margin-bottom: -5px;
+      color: rgb(60, 0, 128);
+      font-weight: 700;
     }
   }
 </style>
